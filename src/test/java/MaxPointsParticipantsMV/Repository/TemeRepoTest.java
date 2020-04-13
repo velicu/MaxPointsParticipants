3782946b0acd2bb5b002c59884172e67e99fe766
Szilard Veress <Szilard.Veress@cloudflight.io>
2020-04-13 19:55:51 +0300

    all wbt testcases

--- src/test/java/MaxPointsParticipantsMV/Repository/TemeRepoTest.java
+++ src/test/java/MaxPointsParticipantsMV/Repository/TemeRepoTest.java
@@ -36,6 +36,15 @@ public class TemeRepoTest {
     }
 
     @Test
+    public void saveAssignmentBadID2() {
+        TemeRepo repo = new TemeRepo(new TemeValidator(), "teme.xml");
+        Teme tema = new Teme(null, "asta este o tema", 10, 12);
+        assertThrows(ValidationException.class, () -> {
+            repo.save(tema);;
+        });
+    }
+
+    @Test
     public void saveAssignmentBadDeadline1() {
         TemeRepo repo = new TemeRepo(new TemeValidator(), "teme.xml");
         Teme tema = new Teme(1, "asta este o tema", 10, 20);
@@ -45,9 +54,36 @@ public class TemeRepoTest {
     }
 
     @Test
+    public void saveAssignmentBadDeadline2() {
+        TemeRepo repo = new TemeRepo(new TemeValidator(), "teme.xml");
+        Teme tema = new Teme(1, "asta este o tema", 10, 0);
+        assertThrows(ValidationException.class, () -> {
+            repo.save(tema);;
+        });
+    }
+
+    @Test
+    public void saveAssignmentBadDeadline3() {
+        TemeRepo repo = new TemeRepo(new TemeValidator(), "teme.xml");
+        Teme tema = new Teme(1, "asta este o tema", 10, 8);
+        assertThrows(ValidationException.class, () -> {
+            repo.save(tema);;
+        });
+    }
+
+    @Test
     public void saveAssignmentBadSaptamanaDePrimireIGuess1() {
         TemeRepo repo = new TemeRepo(new TemeValidator(), "teme.xml");
         Teme tema = new Teme(1, "asta este o tema", 20, 12);
+        assertThrows(ValidationException.class, () -> {
+            repo.save(tema);;
+        });
+    }
+
+    @Test
+    public void saveAssignmentBadSaptamanaDePrimireIGuess2() {
+        TemeRepo repo = new TemeRepo(new TemeValidator(), "teme.xml");
+        Teme tema = new Teme(1, "asta este o tema", -1, 12);
         assertThrows(ValidationException.class, () -> {
             repo.save(tema);;
         });
