94d969a54f51518dbfab1db8ae572575e1f6802d
Szilard Veress <Szilard.Veress@cloudflight.io>
2020-04-13 19:51:29 +0300

    added some wbt testcases

--- src/test/java/MaxPointsParticipantsMV/Repository/TemeRepoTest.java
+++ src/test/java/MaxPointsParticipantsMV/Repository/TemeRepoTest.java
@@ -24,4 +24,32 @@ public class TemeRepoTest {
             repo.save(tema);;
         });
     }
+
+    // FOR WBT:
+    @Test
+    public void saveAssignmentBadID() {
+        TemeRepo repo = new TemeRepo(new TemeValidator(), "teme.xml");
+        Teme tema = new Teme(-1, "asta este o tema", 10, 12);
+        assertThrows(ValidationException.class, () -> {
+            repo.save(tema);;
+        });
+    }
+
+    @Test
+    public void saveAssignmentBadDeadline() {
+        TemeRepo repo = new TemeRepo(new TemeValidator(), "teme.xml");
+        Teme tema = new Teme(1, "asta este o tema", 10, 20);
+        assertThrows(ValidationException.class, () -> {
+            repo.save(tema);;
+        });
+    }
+
+    @Test
+    public void saveAssignmentBadSaptamanaDePrimireIGuess() {
+        TemeRepo repo = new TemeRepo(new TemeValidator(), "teme.xml");
+        Teme tema = new Teme(1, "asta este o tema", 20, 12);
+        assertThrows(ValidationException.class, () -> {
+            repo.save(tema);;
+        });
+    }
 }
